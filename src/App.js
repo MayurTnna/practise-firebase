531e53f9ac05a55335659c504022a232f54df07a
mayurtnn <mayurt@zignuts.com>
2023-04-26 16:04:59 +0530

    setup and connection with db success

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { getDatabase, ref, set } from "firebase/database";
+import { app } from "./firebase";
 
+const db = getDatabase(app);
 function App() {
+  const putData = () => {
+    set(ref(db, "users/mayur"), {
+      id: 1,
+      name: "mayur",
+      age: 21,
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      <button onClick={putData}>Click</button>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDJghzsiPUyKPb8zLpPmVlZ4Kg8lHum2YY",
+  authDomain: "practise-project-b7874.firebaseapp.com",
+  projectId: "practise-project-b7874",
+  storageBucket: "practise-project-b7874.appspot.com",
+  messagingSenderId: "1094366997357",
+  appId: "1:1094366997357:web:b220918d7e68d625c10864",
+  dataBaseUrl: "https://practise-project-b7874-default-rtdb.firebaseio.com/",
+};
+export const app = initializeApp(firebaseConfig);
